16167a6c5a666dfc59d0d7025d2f4ebf534d7a8b
Mohammad Reza Moradi <mohammadrezamoradi382@gmail.com>
2024-04-29 10:15:13 -0600

    changed..

--- mission.js
+++ mission.js
@@ -11,10 +11,10 @@ function changeTheme() {
     // Change the source of the logo to point to the white logo
     document.getElementById("logo").src = "byui-logo_white.webp";
   } else {
-    // If the selected theme is not "dark"
-    // Remove the dark class from the body
+  
+
     document.body.classList.remove("dark");
-    // Change the source of the logo to the blue logo
+    
     document.getElementById("logo").src = "logo.webp";
   }
 }
